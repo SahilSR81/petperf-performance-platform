160a11d3acd2a9f47b282840dde31591c162d841
Sahil Singh <sahilrajputsingh8@gmail.com>
2026-07-31 18:58:18 +0530

    refactor: standardize request naming across workloads

--- locust/tasks/pet_read_tasks.py
+++ locust/tasks/pet_read_tasks.py
@@ -1,6 +1,7 @@
 from locust import task
 
 from utils.data_loader import random_pet, random_status
+from utils.request_names import RequestName, get_request_name
 from utils.validators import validate_status_code
 
 
@@ -8,7 +9,11 @@ class PetReadTasks:
     @task
     def list_pets(self):
         status = random_status()
-        with self.client.get(f"/api/v3/pet/findByStatus?status={status}", catch_response=True, name="list_pets") as resp:
+        with self.client.get(
+            f"/api/v3/pet/findByStatus?status={status}",
+            catch_response=True,
+            name=get_request_name(RequestName.GET_AVAILABLE_PETS),
+        ) as resp:
             validate_status_code(resp, 200)
 
     @task
@@ -17,12 +22,16 @@ class PetReadTasks:
         with self.client.get(
             f"/api/v3/pet/findByStatus?status={status}",
             catch_response=True,
-            name="find_by_status",
+            name=get_request_name(RequestName.GET_AVAILABLE_PETS),
         ) as resp:
             validate_status_code(resp, 200)
 
     @task
     def get_pet_by_id(self):
         pet = random_pet()
-        with self.client.get(f"/api/v3/pet/{pet['id']}", catch_response=True, name="get_pet_by_id") as resp:
+        with self.client.get(
+            f"/api/v3/pet/{pet['id']}",
+            catch_response=True,
+            name=get_request_name(RequestName.GET_PET_BY_ID),
+        ) as resp:
             validate_status_code(resp, 200)
