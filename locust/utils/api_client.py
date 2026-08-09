157a8e9ad1c5aebd33948cf703065581b69ad82f
Sahil Singh <sahilrajputsingh8@gmail.com>
2026-08-09 15:59:51 +0530

    feat: add HTTP response classification

--- locust/utils/api_client.py
+++ locust/utils/api_client.py
@@ -1,5 +1,9 @@
+from contextlib import contextmanager
+from typing import Iterator
+
 from locust.clients import ResponseContextManager
 
+from .http_status import is_client_error, is_server_error
 from .request_context import RequestContext
 from .retry import get_retry_policy
 
@@ -9,6 +13,7 @@ class ApiClient:
         self.client = client
         self.context = RequestContext()
 
+    @contextmanager
     def get(
         self,
         endpoint: str,
@@ -16,13 +21,19 @@ class ApiClient:
         params=None,
         name=None,
         catch_response=True,
-    ) -> ResponseContextManager:
+    ) -> Iterator[ResponseContextManager]:
         policy = get_retry_policy()  # noqa: F841
 
-        return self.client.get(
+        with self.client.get(
             endpoint,
             params=params,
             name=name,
             headers=self.context.build_headers(),
             catch_response=catch_response,
-        )
+        ) as response:
+            if is_server_error(response.status_code):
+                response.failure("Server error")
+            elif is_client_error(response.status_code):
+                response.failure("Client error")
+
+            yield response
